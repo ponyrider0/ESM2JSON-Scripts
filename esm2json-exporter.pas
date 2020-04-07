96fa697aa569fd6f2f20b0faaf02bb2f223132f6
Daniel Bui <danielbui78@gmail.com>
2020-04-07 16:22:47 -0700

    hardcode skip CELL, WRLD

--- esm2json-exporter.pas
+++ esm2json-exporter.pas
@@ -871,6 +871,7 @@ begin
   json_filecount := json_filecount + 1;
   if (json_filecount mod 100 = 0) then AddMessage('INFO: ' + IntToStr(json_filecount) + ' files written...');
 //  json_output := TStringList.Create;
+  if ( (Pos('WRLD', file_path) = 1) Or (Pos('CELL', file_path) = 1) ) then exit;
   ProcessRecord(e, '', '');
   ForceDirectories(PROGRAMPATH + '\' + file_path);
   json_output.SaveToFile(PROGRAMPATH + '\' + file_path + element_filename);
