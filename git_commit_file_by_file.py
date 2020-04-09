97ae41de841f6e260587f3f9655c26dde2548c74
Daniel Bui <danielbui78@gmail.com>
2020-04-09 11:51:01 -0700

    added "--untracked-files=all" to git status

--- git_commit_file_by_file.py
+++ git_commit_file_by_file.py
@@ -2,6 +2,8 @@ import sys
 import os
 import subprocess
 
+git_exe = "git"
+
 if len(sys.argv) != 2:
     print "Please specify commit message to continue."
     exit()
@@ -9,7 +11,7 @@ commit_title = sys.argv[1]
 #print "DEBUG: commit message is: \"" + commit_title + "\""
 
 #print "DEBUG: Running 'git status --porcelain=1' ..."
-result_array = subprocess.check_output(["git", "status", "--porcelain=1"])
+result_array = subprocess.check_output(["git", "status", "--porcelain=1", "--untracked-files=all"])
 result_lines = result_array.splitlines()
 #print "DEBUG: Number of files to commit: " + str(len(result_lines))
 i=0
@@ -21,7 +23,7 @@ for line in result_lines:
         operator=line[0:2]
         files=line[3:].split("\0")
 #        print "DEBUG: operator=\"" + operator + "\""
-#        print "DEBUG: file=\"" + files[0] + "\""
+        print "DEBUG: file=\"" + files[0] + "\""
 #        if len(files) > 1:
 #            print "DEBUG: orig_file=" +files[1]
         if operator[0] != ' ' and operator[0] != '?':
@@ -30,24 +32,25 @@ for line in result_lines:
         if operator[1] == 'D':
 #            print "DEBUG: file marked for deletion"
 #            print "DEBUG: git rm " + files[0]
-            subprocess.call(["git", "rm", files[0].strip('\"')])
+            subprocess.call([git_exe, "rm", files[0].strip('\"')])
             commit_message = "Removed " + files[0]
         elif operator[1] == 'M':
 #            print "DEBUG: file marked as modified"
 #            print "DEBUG: git add " + files[0]
-            subprocess.call(["git", "add", files[0].strip('\"')])
+            subprocess.call([git_exe, "add", files[0].strip('\"')])
             commit_message = "Modified " + files[0]
         elif operator[1] == '?':
 #            print "DEBUG: file marked as untracked"
 #            print "DEBUG: git add " + files[0]
-            subprocess.call(["git", "add", files[0].strip('\"')])
+            subprocess.call([git_exe, "add", files[0].strip('\"')])
             commit_message = "Added " + files[0]
         else:
             print "ERROR: unhandled file status: " + operator[1]
             exit()
         commit_message = commit_title + ": " + commit_message
 #        print "DEBUG: git commit -m \"" + commit_message + "\""
-        subprocess.call(["git", "commit", "-m", commit_message])
+        subprocess.call([git_exe, "commit", "-m", commit_message])
+        raw_input("Press ENTER to continue.")
     else:
         print "ERROR: parsing output on line[" + str(i) + "]: " + line
     i += 1
