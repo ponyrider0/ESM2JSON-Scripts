3a6b01978a5b0e57337a59113c7bd0a64fdb085d
Daniel Bui <danielbui78@gmail.com>
2020-04-09 08:08:07 -0700

    Create git_commit_file_by_file.py

---
+++ git_commit_file_by_file.py
@@ -0,0 +1,52 @@
+import sys
+import os
+import subprocess
+
+if len(sys.argv) != 2:
+    print "Please specify commit message"
+    exit()
+commit_title = sys.argv[1]
+print "DEBUG: commit message is: \"" + commit_title + "\""
+
+print "DEBUG: Current working directory: " + os.getcwd()
+print "DEBUG: Running 'git status --porcelain=1' ..."
+result_array = subprocess.check_output(["git", "status", "--porcelain=1"])
+result_lines = result_array.splitlines()
+print "DEBUG: Number of files to commit: " + str(len(result_lines))
+i=0
+for line in result_lines:
+    if line == "":
+        continue
+    print "DEBUG: line[" + str(i) + "]=" + line
+    if len(line) > 4:
+        operator=line[0:2]
+        files=line[3:].split("\0")
+        print "DEBUG: operator=\"" + operator + "\""
+        print "DEBUG: file=\"" + files[0] + "\""
+        if len(files) > 1:
+            print "DEBUG: orig_file=" +files[1]
+        if operator[0] != ' ' and operator[0] != '?':
+            print "ERROR: existing staging detected, please commit or revert staging before running script."
+            exit()
+        if operator[1] == 'D':
+            print "DEBUG: file marked for deletion"
+            print "DEBUG: git rm " + files[0]
+            commit_message = "Removed " + files[0]
+        elif operator[1] == 'M':
+            print "DEBUG: file marked as modified"
+            print "DEBUG: git add " + files[0]
+            commit_message = "Modified " + files[0]
+        elif operator[1] == '?':
+            print "DEBUG: file marked as untracked"
+            print "DEBUG: git add " + files[0]
+            commit_message = "Added " + files[0]
+        else:
+            print "ERROR: unhandled file status: " + operator[1]
+            exit()
+        commit_message = commit_title + ": " + commit_message
+        print "DEBUG: git commit -m \"" + commit_message + "\""
+    else:
+        print "DEBUG: error parsing output on line[" + str(i) + "]: " + line
+    if i > 100:
+        break
+    i += 1
